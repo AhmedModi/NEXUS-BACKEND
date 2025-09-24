b57bc4b1bbccd7fbd80bb4c67c9f176aeeb9fe03
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-09-24 19:22:17 +0100

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import health_check
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -36,4 +37,7 @@ urlpatterns = [
 
     # App routes
     path('api/', include('shop.urls')),
+
+    # Health check
+    path("health/", health_check),
 ]
